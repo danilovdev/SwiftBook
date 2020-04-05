baf42eddf996f77d94498fc524c40674033ba85b
Данилов Алексей Николаевич <ANDanilov@alfabank.ru>
2020-04-05 12:02:19 +0300

    Course 37. Collections App. Initial commit.

---
+++ Courses/37_CollectionsApp/EmojiReader/EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Aleksei Danilov on 05.04.2020.
+//  Copyright © 2020 DanilovDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
